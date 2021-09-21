8670e355e4611c35d6476d69bc225531eb8647a5
subastianSk <subastian.sk@gmail.com>
2021-09-21 02:38:09 -0400

    javascript

--- 06-Javascript-Dasar/04-loop/04-loop.js
+++ 06-Javascript-Dasar/04-loop/04-loop.js
@@ -1,21 +1,33 @@
 /// Soal - 01
 /// Kamu diminta untuk membuat sebuah perulangan menggunakan for loop, untuk mencetak angka prima dari 1 ~ 100
 /// EDIT HERE
-for (....) { }
+for (let i=1;i<=100;i++) {
+    console.log("angka"+i);
+ }
 
 /// Soal - 02
 /// Kamu diminta untuk menemukan bilangan prima ke-50, temukan bilangan tersebut menggunakan while loop
 
-let primeCounter = 0;
-const fiftiethPrime;
+let primeCounter = 50;
+const fiftiethPrime="";
 
 /// EDIT HERE
-while (....) {}
+while (primeCounter<=50) {
+    console.log("Bilangan Prima"+primeCounter);
+    primeCounter++;
+}
 
 
 /// Soal - 03
 /// Kamu diminta untuk menemukan bilangan ganjil ke-50, temukan bilangan tersebut menggunakan do while loop
 
-let oddCounter = 0;
-const fiftiethOdd;
-do { .... } while (....)
+let oddCounter = 50;
+const fiftiethOdd=ganjil;
+do {
+    oddCounter;
+    if (oddCounter % 2 ==0) {
+        console.log("Genap");
+    }else{
+        console.log("Ganjil");
+    }
+} while(oddCounter <= 50);
