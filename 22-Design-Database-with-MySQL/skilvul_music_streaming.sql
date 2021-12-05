b5b35e1010d484cb4b1550d04517a8f9667402b5
subastianSk <subastian.sk@gmail.com>
2021-12-05 08:56:29 -0500

    done

---
+++ 22-Design-Database-with-MySQL/skilvul_music_streaming.sql
@@ -0,0 +1,254 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Des 2021 pada 14.27
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `skilvul_music_player`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `album`
+--
+
+CREATE TABLE `album` (
+  `album_id` int(10) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `singer_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `album`
+--
+
+INSERT INTO `album` (`album_id`, `name`, `singer_id`) VALUES
+(1, 'Monokrom', 1),
+(2, 'AugustD', 2),
+(3, 'Noeasy', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `playlist_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `playlist`
+--
+
+INSERT INTO `playlist` (`playlist_id`, `name`, `user_id`) VALUES
+(1, 'Study', 1),
+(2, 'Favorite', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `playlist_track`
+--
+
+CREATE TABLE `playlist_track` (
+  `playlist_id` int(11) NOT NULL,
+  `track_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `playlist_track`
+--
+
+INSERT INTO `playlist_track` (`playlist_id`, `track_id`) VALUES
+(1, 1),
+(1, 4),
+(2, 3),
+(2, 6),
+(2, 1),
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `singer`
+--
+
+CREATE TABLE `singer` (
+  `singer_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `singer`
+--
+
+INSERT INTO `singer` (`singer_id`, `name`) VALUES
+(1, 'Tulus'),
+(2, 'Suga'),
+(3, 'Straykids');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `track`
+--
+
+CREATE TABLE `track` (
+  `track_id` int(10) NOT NULL,
+  `title` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `singer` int(10) NOT NULL,
+  `album` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `track`
+--
+
+INSERT INTO `track` (`track_id`, `title`, `singer`, `album`) VALUES
+(1, 'Pamit', 1, 1),
+(2, 'The Last', 2, 2),
+(3, 'Lekas', 1, 1),
+(4, 'Thunderous', 3, 3),
+(5, 'Cheese', 3, 3),
+(6, 'Tukar Jiwa', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(50) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`user_id`, `name`, `email`, `password`) VALUES
+(1, 'Devi', 'devialestari@gmail.com', 'devi123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`album_id`) USING BTREE,
+  ADD KEY `fk_singer_id` (`singer_id`);
+
+--
+-- Indeks untuk tabel `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`playlist_id`),
+  ADD KEY `user` (`user_id`);
+
+--
+-- Indeks untuk tabel `playlist_track`
+--
+ALTER TABLE `playlist_track`
+  ADD KEY `playlist` (`playlist_id`),
+  ADD KEY `track` (`track_id`);
+
+--
+-- Indeks untuk tabel `singer`
+--
+ALTER TABLE `singer`
+  ADD PRIMARY KEY (`singer_id`);
+
+--
+-- Indeks untuk tabel `track`
+--
+ALTER TABLE `track`
+  ADD PRIMARY KEY (`track_id`),
+  ADD KEY `album` (`album`),
+  ADD KEY `singer` (`singer`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `singer`
+--
+ALTER TABLE `singer`
+  MODIFY `singer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `track`
+--
+ALTER TABLE `track`
+  MODIFY `track_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `fk_singer_id` FOREIGN KEY (`singer_id`) REFERENCES `singer` (`singer_id`);
+
+--
+-- Ketidakleluasaan untuk tabel `playlist`
+--
+ALTER TABLE `playlist`
+  ADD CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `playlist_track`
+--
+ALTER TABLE `playlist_track`
+  ADD CONSTRAINT `playlist` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`playlist_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `track` FOREIGN KEY (`track_id`) REFERENCES `track` (`track_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `track`
+--
+ALTER TABLE `track`
+  ADD CONSTRAINT `album` FOREIGN KEY (`album`) REFERENCES `album` (`album_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `singer` FOREIGN KEY (`singer`) REFERENCES `singer` (`singer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
