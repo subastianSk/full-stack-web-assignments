e9a5130f520f6ba7a79ef5fdfdd1c1c90f12a9ea
subastianSk <subastian.sk@gmail.com>
2021-12-06 04:45:31 -0500

    blm selesai

---
+++ 26-Authentication-and-Authorization/soal1/app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+
+const app = express();
+const PORT = 3000;
+
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+
+app.use(sessions({
+    secret: "ahahahahkkaahks",
+    saveUninitialized: true,
+    cookie: {
+        maxAge: oneDay
+    },
+    resave: false
+}));
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+app.use(express.static(__dirname));
+
+
+app.use(cookieParser());
+
+
+const myusername = 'user1'
+const mypassword = 'mypassword'
+
+
+var session;
+
+app.get('/', (req, res) => {
+    session = req.session;
+    if (session.userid) {
+        res.send("Welcome User <a href=\'/logout'>click to logout</a>");
+    } else
+        res.sendFile('views/index.html', {
+            root: __dirname
+        })
+});
+
+app.post('/user', (req, res) => {
+    if (req.body.username == myusername && req.body.password == mypassword) {
+        session = req.session;
+        session.userid = req.body.username;
+        console.log(req.session)
+        res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
+    } else {
+        res.send('Invalid username or password');
+    }
+})
+
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
