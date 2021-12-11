9397b6f9e80d911863ca5f230c19ff2ad1a89b89
subastianSk <subastian.sk@gmail.com>
2021-12-11 08:27:17 -0500

    blm fix

---
+++ 26-Authentication-and-Authorization/soal2/auth.js
@@ -0,0 +1,83 @@
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+
+const app = express()
+const port = 3000
+
+const users = [{
+    username: 'terra',
+    password: 'password123admin',
+    role: 'admin'
+}, {
+    username: 'dave',
+    password: 'password123member',
+    role: 'member'
+}];
+
+//middleware
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+
+function MustLoggedinMiddleware(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect("/");
+};
+
+
+
+dotenv.config()
+app.use(bodyParser.json())
+
+app.post("/login", (req, res) => {
+    const {
+        username,
+        password
+    } = req.body
+    const user = users.find(user => {
+        return ((user.username === username) && (user.password === password))
+    })
+
+    if (user) {
+        const token = jwt.sign({
+            username: user.username,
+            role: user.role
+        }, process.env.TOKEN_SECRET)
+        res.json({
+            token
+        })
+    } else {
+        res.send("Username or password incorrect")
+    }
+})
+
+app.get("/users", authenticateJWT, (req, res) => {
+    res.send(users)
+})
+
+app.listen(port, () => {
+    console.log("server is listening on port ", port);
+});
+
+exports.MustLoggedinMiddleware = MustLoggedinMiddleware;
---
+++ 26-Authentication-and-Authorization/soal2/books.js
@@ -0,0 +1,45 @@
+const MustLoginMW = auth.MustLoggedinMiddleware;
+
+const books = [{
+        "author": "Robert Martin",
+        "country": "USA",
+        "language": "English",
+        "pages": 209,
+        "title": "Clean Code",
+        "year": 2008
+    },
+    {
+        "author": "Dave Thomas & Andy Hunt",
+        "country": "USA",
+        "language": "English",
+        "pages": 784,
+        "title": "The Pragmatic Programmer",
+        "year": 1999
+    },
+    {
+        "author": "Kathy Sierra, Bert Bates",
+        "country": "USA",
+        "language": "English",
+        "pages": 928,
+        "title": "Head First Java",
+        "year": 2003
+    },
+];
+
+app.get("/books", MustLoginMW, (req, res) => {
+    res.send(books);
+});
+
+app.post("/books", MustLoginMW, (req, res) => {
+    const newBooks = {
+        author: 'Kyle Simpson',
+        country: 'USA',
+        language: 'English',
+        pages: 226,
+        title: "You don't know JS",
+        year: 2014
+    };
+
+    books.push(newBooks);
+    res.send(newBooks);
+});
